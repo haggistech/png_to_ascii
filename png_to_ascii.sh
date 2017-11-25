bf311b801ad7b2d174d761759b7b346dbea612cc
Mik McLean <haggistech@gmail.com>
2017-11-25 21:02:07 +0000

    Version 1

--- png_to_ascii.sh
+++ png_to_ascii.sh
@@ -11,10 +11,25 @@
 #
 #==========================================================================================================================
 
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' jp2a 2>/dev/null | grep "install ok installed" | wc -l)
+echo
+if [ $PKG_OK -eq 0 ] ; then
+echo "JP2A not found, please install JP2A"
+echo
+sleep 2
+exit
+fi
 
+numfiles=$(ls | wc -l)
 
+if [ $numfiles \> 1 ] ; then
 for logo in *.png; do
     echo "Logo: $logo"
-    convert $logo jpg:- | jp2a --background=light --colors --height=20 -
+    convert $logo jpg:- | jp2a --background=dark --colors --height=20 -
 done
+else
+echo "No Input Files"
+echo
+exit
+fi
 
